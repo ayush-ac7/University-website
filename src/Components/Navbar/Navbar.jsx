8d333bf9037324f01a558e0c761d94fe7c85664a
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-03-03 12:35:10 +0530

    done with navbar and homepage section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,15 +3,15 @@ import logo from '../../assets/logo.png'
 
 export default function Navbar() {
     return (
-        <nav>
-            <img src={logo} alt="" />
+        <nav className="container">
+            <img src={logo} alt="" className="logo" />
             <ul>
                 <li>Home</li>
                 <li>Program</li>
                 <li>About Us</li>
                 <li>Campus</li>
                 <li>Testimonials</li>
-                <li>Contact us</li>
+                <li><button className="btn">Contact us</button></li>
             </ul>
 
         </nav>
