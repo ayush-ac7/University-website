22806fc2993bf4e306eca2a0ee82ebf483c44122
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-03-03 23:36:39 +0530

    created program component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Program/Programs";
+import Title from "./Components/Title/Title";
 
 export default function App() {
 
@@ -8,8 +9,10 @@ export default function App() {
     <div>
       <Navbar />
       <Hero />
-      <Programs />
-        
+      <div className="container">
+        <Title/>
+       <Programs />
+      </div> 
     </div>
    )
 }
---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,11 @@
+import "./Title.css";
+
+export default function Title() {
+    return (
+        <div className="title">
+            <p>Our PROGRAM</p>
+            <h2>What We Offer</h2>
+        </div>
+    )
+    
+}
