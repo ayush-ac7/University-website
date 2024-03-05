909db852dde3cacb7fc42ca909a83a8c338fbcd0
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-03-05 23:19:41 +0530

    created campus component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Program/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 
 export default function App() {
 
@@ -12,8 +13,10 @@ export default function App() {
       <Hero />
       <div className="container">
         <Title subTitle="Our PROGRAM" title="What We Offer"/>
-       <Programs />
-       <About />
+        <Programs />
+        <About />
+        <Title subTitle="GALLERY" title="Campus Photos"/>
+        <Campus />
       </div> 
     </div>
    )
