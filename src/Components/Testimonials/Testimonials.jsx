04d8359450af84124ca7d485d9961a8751054ee2
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-03-07 23:14:03 +0530

    created testimonial component

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,13 +1,84 @@
 import "./Testimonials.css"
 import back_icon from "../../assets/back-icon.png"
 import next_icon from "../../assets/next-icon.png"
+import user_1 from "../../assets/user-1.png"
+import user_2 from "../../assets/user-2.png"
+import user_3 from "../../assets/user-3.png"
+import user_4 from "../../assets/user-4.png"
 
 export default function Testimonials() {
     return (
         <div className="testimonials">
             <img src={back_icon} alt="" className="back-btn" />
             <img src={next_icon} alt="" className="next-btn" />
-            <div className="slider"></div>
+            <div className="slider">
+                <ul>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_1} alt="" />
+                            </div>
+                            <div>
+                                <h3>Willian Singh</h3>
+                                <span>Edusity, Mirzapur</span>
+                            </div>
+                        </div>
+                        <p>Choosing to persue my degree at Edusity was one of
+                            the best decision I've ever made. The supportive
+                            community, state-of-the-art facilities, and commitment
+                            to academic excellence have truly exceeded my
+                            expectations.</p>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_2} alt="" />
+                            </div>
+                            <div>
+                                <h3>Jon Kumar Wesley</h3>
+                                <span>Edusity, Delhi</span>
+                            </div>
+                        </div>
+                        <p>Choosing to persue my degree at Edusity was one of
+                            the best decision I've ever made. The supportive
+                            community, state-of-the-art facilities, and commitment
+                            to academic excellence have truly exceeded my
+                            expectations.</p>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_3} alt="" />
+                            </div>
+                            <div>
+                                <h3>Hermoine Gupta</h3>
+                                <span>Edusity, Itawa</span>
+                            </div>
+                        </div>
+                        <p>Choosing to persue my degree at Edusity was one of
+                            the best decision I've ever made. The supportive
+                            community, state-of-the-art facilities, and commitment
+                            to academic excellence have truly exceeded my
+                            expectations.</p>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_4} alt="" />
+                            </div>
+                            <div>
+                                <h3>Harry Puttar</h3>
+                                <span>Edusity, Punjab</span>
+                            </div>
+                        </div>
+                        <p>Choosing to persue my degree at Edusity was one of
+                            the best decision I've ever made. The supportive
+                            community, state-of-the-art facilities, and commitment
+                            to academic excellence have truly exceeded my
+                            expectations.</p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
